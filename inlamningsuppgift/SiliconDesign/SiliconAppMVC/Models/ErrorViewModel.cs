733371666db7b7ebf4ffb3a55dd0b00015136c6b
Duker <m.oliver.henriksson@gmail.com>
2024-02-16 16:23:18 +0100

    Added base solution with default Aspnet MVC project

---
+++ inlamningsuppgift/SiliconDesign/SiliconAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace SiliconAppMVC.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
